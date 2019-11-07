13793bb021c5b6c3338c0d035f60bd0047d44b82
Fomotoshi <fomotoshi@Fomotoshis-iMac.local>
2019-11-07 18:06:23 +0300

    lesson 10 complete

---
+++ docker-laravel db Session.sql
@@ -0,0 +1,20 @@
+show tables;
+
+select * from users;
+select * from blog_categories;
+select * from blog_posts;
+
+
+
+
+drop table blog_categories;
+
+drop table blog_posts;
+
+drop table migrations;
+
+drop table users;
+
+drop table failed_jobs;
+
+drop table password_resets;
