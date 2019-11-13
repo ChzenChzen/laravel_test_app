fe37821bb683d8c0ae6a30a1f9927837da23e1e2
Fomotoshi <fomotoshi@Fomotoshis-iMac.local>
2019-11-13 11:42:51 +0300

    lesson 7. edit page for categories

---
+++ resources/views/blog/admin/category/includes/item_edit_main_col.blade.php
@@ -0,0 +1,50 @@
+@php /** @var \App\Models\BlogCategory $item */ @endphp
+
+<div class="row justify-content-center">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="card-title"></div>
+                <ul class="nav nav-tabs" role="tablist">
+                    <li class="nav-item">
+                        <a href="#maindata" class="nav-link active" data-toggle="tab" role="tab">Main data</a>
+                    </li>
+                </ul>
+                <br>
+                <div class="tab-content">
+                    <div class="tab-pane active" id="maindata" role="tabpanel">
+                        <div class="form-group">
+                            <label for="title">Header</label>
+                            <input value="{{ $item->title }}" name="title" id="title" type="text" class="form-control"
+                                minlength="3" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="slug">Indentificator</label>
+                            <input name="slug" value="{{ $item->slug }}" id="slug" type="text" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="parent_id">Parent</label>
+                            <select name="parent_id" id="parent_id" class="form-control" placeholer="Choose category"
+                                required="required">
+                                @foreach ($categoryList as $categoryOption)
+                                <option value="{{ $categoryOption->id }}" @if ($categoryOption->id == $item->parent_id)
+                                    selected @endif>
+                                    {{ $categoryOption->id }}. {{ $categoryOption->title }}
+                                </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="description">Description</label>
+                            <textarea name="description" id="description" cols="30" rows="3" class="form-control">
+                                {{ $item->description }}
+                            </textarea>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
