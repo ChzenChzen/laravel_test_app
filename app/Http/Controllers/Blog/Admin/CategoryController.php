0ee547b956b7af688eee8fe52a92f87c864aabb4
fomotoshi <fomotoshi@ya.ru>
2019-11-16 10:21:31 +0300

    lesson 22. making separated layer in Requests for validators

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Blog\Admin;
 
 use App\Models\BlogCategory;
 use Illuminate\Http\Request;
+use App\Http\Requests\BlogCategoryUpdateRequest;
 
 class CategoryController extends BaseController
 {
@@ -61,18 +62,18 @@ class CategoryController extends BaseController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(BlogCategoryUpdateRequest $request, $id)
     {
-        $rules = [
-            'title' => 'required|min:5|max:200',
-            'slug' => 'max:200',
-            'description' => 'string|max:500|min:3',
-            'parent_id' => 'required|integer|exists:blog_categories,id',
-        ];
+        // $rules = [
+        //     'title' => 'required|min:5|max:200',
+        //     'slug' => 'max:200',
+        //     'description' => 'string|max:500|min:3',
+        //     'parent_id' => 'required|integer|exists:blog_categories,id',
+        // ];
 
-        $validatedData = $request->validate($rules);
+        // $validatedData = $request->validate($rules);
 
-        dd($validatedData);
+        // dd($validatedData);
 
         $item = BlogCategory::find($id);
 
